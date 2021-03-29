e2da252f94e2d899325337c6bd966c50764a0a91
EMIKEI <72548507+EMIKEI@users.noreply.github.com>
2021-03-29 16:18:49 +0900

    change UI

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const loadingPage = document.querySelector(".loadingPage")
-const selectFoodZone = document.querySelector(".selectFoodZone");
 const foodButs = document.querySelector(".foodButs");
 const foodBut = document.querySelectorAll(".foodBut");
-const mainZone = document.querySelector(".mainZone");
+const foodImgZone = document.querySelector(".foodImgZone");
 const foodImg = document.querySelector(".foodImg");
+const alertFoodImg = document.querySelector(".alertFoodImg");
+const closeFoodImg = document.querySelector(".foodImgZone button");
 const storeBut = document.querySelector(".storeBut")
 
-const scrollFoodZone = document.querySelectorAll(".scrollFoodZone");
-const selectFoodZoneWidth = selectFoodZone.offsetWidth;
-
 const foodImgBackgroundImg = [
-    "url('https://han.gl/XhaeW')", 
-    "url('http://collectionpng.com/images/13732.png')",
-    "url('http://kukde.co.kr/wordpress/wp-content/uploads/2016/05/tt.png')",
+    'https://han.gl/XhaeW', 
+    'http://collectionpng.com/images/13732.png',
+    'https://han.gl/QB4GK'
 ];
-//lodingPage
-loadingPage.style.display="flex";
+
 
 window.addEventListener("load", function(){
     setTimeout(function(){
@@ -23,58 +20,16 @@ window.addEventListener("load", function(){
     },2000)
 })
 
-// click foodBut
 for(let i = 0; i< foodBut.length; i++ ){
     foodBut[i].addEventListener("click", function(){
-
-        for(let i2 = 0; i2 < foodBut.length; i2++){
-            foodBut[i2].classList.remove('selectedBut')
-        }
-        foodBut[i].classList.add('selectedBut')
-        foodImg.style.backgroundImage = foodImgBackgroundImg[i]; 
+        foodImg.src = foodImgBackgroundImg[i];
+        alertFoodImg.style.display = "flex";
     })
 }
 
-
-//scroll foodButs
-const foodButsWidth = foodButs.offsetWidth;
-
-// 왼쪽 scrollFoodZone
-scrollFoodZone[0].addEventListener("mouseenter", function(){
-    foodButs.scrollBy({
-        left: -foodButsWidth/4,
-        behavior:"smooth"
-    })
-    scrollfoodZone1=
-    setInterval(function(){
-        foodButs.scrollBy({
-            left: -foodButsWidth/4,
-            behavior:"smooth"
-        })
-    },470)
+closeFoodImg.addEventListener("click", function(){
+    alertFoodImg.style.display = "none";
 })
-scrollFoodZone[0].addEventListener("mouseout",function(){
-    clearInterval(scrollfoodZone1)
-})
-//오른쪽 scrollFoodZone
-scrollFoodZone[1].addEventListener("mouseenter", function(){
-        foodButs.scrollBy({
-            left: foodButsWidth/4,
-            behavior:"smooth"
-        })
-        scrollfoodZone2=
-        setInterval(function(){
-            foodButs.scrollBy({
-                left: foodButsWidth/4,
-                behavior:"smooth"
-            })
-        },470)
-})
-scrollFoodZone[1].addEventListener("mouseout",function(){
-    clearInterval(scrollfoodZone2)
-})
-
-//storeBut
 
 storeBut.addEventListener("click", function(){
     alert("준비 중")
