59f3912a609593712b36e0a8efbd57e051929b0a
EMIKEI <72548507+EMIKEI@users.noreply.github.com>
2021-07-02 18:53:31 +0900

    change design

--- main.js
+++ main.js
@@ -1,72 +1,37 @@
-const main = document.querySelector("main")
-const foodButZone = document.querySelector(".foodButZone")
 const foodBut = document.querySelectorAll(".foodBut")
-const foodImgZone = document.querySelector(".foodImgZone")
-const foodImg = document.querySelector(".foodImgZone img")
 
-const foodButImg = {
-    pizza: 'https://han.gl/XhaeW', 
-    chicken: 'http://collectionpng.com/images/13732.png',
-    tteokbokki: 'https://han.gl/QB4GK',
-    burger: 'https://han.gl/kZVeg'
+const foodImg = {
+    피자: 'https://han.gl/XhaeW', 
+    치킨: 'http://collectionpng.com/images/13732.png',
+    떡볶이: 'https://han.gl/QB4GK',
+    버거: 'https://han.gl/kZVeg'
 }
-//window load
-{
-    window.addEventListener("load", load)
 
-    function load(){
-        for(let i = 0; i < foodBut.length; i++){
-            foodBut[i].style.backgroundImage = `url(${foodButImg[foodBut[i].name]})`;
-        }
+const foodsKcal = {
+    pizza: "266kcal",
+    chicken: "246kcal",
+    tteokbokki: "300kcal",
+    burger: "295kcal"
+}
+{
+    for(let i=0; i<foodBut.length; i++){
+        foodBut[i].style.backgroundImage = `url(${foodImg[foodBut[i].name]})`;
     }
 }
-//click food button
 {
-    const foodImgZoneText = document.querySelector(".foodImgZone h4");
-    const explainBut = document.querySelector(".explainBut");
+    for(let i=0; i<foodBut.length; i++){
+        foodBut[i].addEventListener("click", clickBut)
+    }
 
-    const foodsKcal = {
-        pizza: "266kcal",
-        chicken: "246kcal",
-        tteokbokki: "300kcal",
-        burger: "295kcal"
+    function clickBut(){
+        alert(this.name)
     }
-    
-    for(let i = 0; i < foodBut.length; i++){
-        foodBut[i].addEventListener("click", click)
-    }
-    
-    function click(){
-        foodImg.classList.remove("hide")
-        explainBut.classList.remove("hide")
-        foodImgZoneText.classList.add("hide")
-        foodImg.src = `${foodButImg[this.name]}`;
-        explainBut.innerHTML = `${this.name} l ${foodsKcal[this.name]}`
-        explainBut.name = this.name;
-    }
-    
-    explainBut.addEventListener("click", function(){
-        alert(`${this.name}의 열량은 100g에 ${foodsKcal[this.name]}입니다.`)
+}
+{
+    const bottom_gradient = document.querySelector(".bottom_gradient")
+    const top = document.querySelector(".top")
+
+    document.addEventListener("scroll", function(){
+        bottom_gradient.classList.add("hide")
     })
 }
-    //mouse over scroll
-{
-    const leftscroll = document.querySelector(".scroll.left")
-    const rightscroll = document.querySelector(".scroll.right")
-    
-    leftscroll.addEventListener("mouseover", function(){
-        foodButZone.scrollBy({
-            left: -foodButZone.offsetWidth/4,
-            behavior: "smooth"
-        })
-    })
-    rightscroll.addEventListener("mouseover", function(){
-        foodButZone.scrollBy({
-            left: foodButZone.offsetWidth/4,
-            behavior: "smooth"
-        })
-    })
-}
-/*
-연속적으로 스크롤하도록 만들어야 함
-*/ 
