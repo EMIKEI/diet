5d795219fefe0748a3cf403e579725ff78990cbb
EMIKEI <72548507+EMIKEI@users.noreply.github.com>
2021-07-06 11:08:54 +0900

    change design

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const foodBut = document.querySelectorAll(".foodBut")
 const foodImg = {
     피자: 'https://han.gl/XhaeW', 
     치킨: 'http://collectionpng.com/images/13732.png',
-    떡볶이: 'https://han.gl/QB4GK',
+    떡볶이: 'https://han.gl/hOvPV',
     버거: 'https://han.gl/kZVeg'
 }
 
@@ -25,26 +25,31 @@ const foodsKcal = {
     const foodZone_img = document.querySelector(".foodZone_img");
     const foodZone_closeBut = document.querySelector(".foodZone_closeBut");
     const bottom_gradient = document.querySelector(".bottom_gradient");
+    const body = document.querySelector("body");
+    let foodName = null;
 
     for(let i=0; i<foodBut.length; i++){
         foodBut[i].addEventListener("click", clickBut);
     }
-
     function clickBut(){
         main.classList.remove("remove");
         foodZone_img.src = foodImg[this.name];
         food_explain.textContent = `${this.name}에 대한 설명`;
-        bottom_gradient.classList.add("hide");
-        document.querySelector("body").classList.add("noScroll")
+        foodName = this.name;
+
+        bottom_gradient.classList.add("remove");
+        body.classList.add("noScroll");
     }
+    
+    food_explain.addEventListener("click", function(){
+        alert(`${foodName}을(를) 섭취하면 ${foodsKcal[foodName]}를 얻습니다.`)
+    })
+    
     foodZone_closeBut.addEventListener("click", function(){
         main.classList.add("remove");
-        document.querySelector("body").classList.remove("noScroll")
+        body.classList.remove("noScroll")
     })
-
-    food_explain.addEventListener("click", function(){
-        alert("준비 중입니다.")
-    })
+    
 }//foodBut,foodZone_closeBut, food_explain click event
 {
     const bottom_gradient = document.querySelector(".bottom_gradient");
@@ -52,5 +57,8 @@ const foodsKcal = {
 
     document.addEventListener("scroll", function(){
         bottom_gradient.classList.add("hide");
+        setTimeout(function(){
+            bottom_gradient.classList.add("remove")
+        }, 700)
     })
 }//bottom_gradient scroll event
